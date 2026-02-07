863dad7d2c3fdb2d0ef1ab8eecbeb9c4d02d4aa4
Logan Mendoza <Loganteodoromendoza@gmail.com>
2026-02-07 13:01:11 -0800

    The .tpp file wont work, had to add to .hpp

--- lab04/LinkedList.hpp
+++ lab04/LinkedList.hpp
@@ -58,5 +58,118 @@ class LinkedList : public List<T> {
         friend ostream& operator<<(ostream&, const LinkedList<U>&);
 };
 
-#include "LinkedList.tpp"
+template <typename T>
+LinkedList<T>::LinkedList()
+    : head(nullptr) {
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList() {
+    clear();
+}
+
+template <typename T>
+void LinkedList<T>::append(const T& elem) {
+    // TODO
+    Node* NewNode = new Node;
+    NewNode->value = elem;
+    NewNode->next = nullptr;
+
+    if (!head) {
+        head = NewNode;
+    }
+
+    else {
+        Node* traveler = head;
+        while (traveler->next != nullptr) {
+            traveler = traveler->next;
+        }
+        traveler->next = NewNode;
+    }
+    this->length++;
+}
+
+template <typename T>
+void LinkedList<T>::clear() {
+    // TODO
+    Node* traveler = head;
+    while (traveler != nullptr) {
+        Node* temp = traveler;
+        traveler = traveler->next;
+        delete temp;
+    }
+
+    head = nullptr;
+    this->length = 0;
+}
+
+template <typename T>
+T LinkedList<T>::getElement(int position) const {
+    // TODO
+    if (this->isEmpty()) {
+        throw string("List is empty, no elements to return\n");
+    }
+
+    if (position < 0 || position >= this->length) {
+        throw string("Index out of bounds");
+    }
+    Node* traveler = head;
+    for (int i = 0; i < position; i++) {
+        traveler = traveler->next;
+    }
+
+    return traveler->value;
+}
+
+template <typename T>
+int LinkedList<T>::getLength() const {
+    return this->length;
+}
+
+template <typename T>
+bool LinkedList<T>::isEmpty() const {
+    return this->length == 0;
+}
+
+template <typename T>
+void LinkedList<T>::replace(int position, const T& elem) {
+    // TODO
+    if (this->isEmpty()) {
+        throw string("List is empty, no elements to replace.\n");
+    }
+
+    if (position < 0 || position >= this->length) {
+        throw string("Index out of bounds");
+    }
+    Node* traveler = head;
+    for (int i = 0; i < position; i++) {
+        traveler = traveler->next;
+    }
+
+    traveler->value = elem;
+}
+
+
+template <typename T>
+ostream& operator<<(ostream& outStream, const LinkedList<T>& myObj) {
+    if (myObj.isEmpty()) {
+        outStream << "List is empty, no elements to display.\n";
+    }
+    else {
+        typename LinkedList<T>::Node* curr = myObj.head;
+        while (curr != nullptr) {
+            outStream << curr->value;
+            if (curr->next != nullptr) {
+                outStream << " --> ";
+            }
+            curr = curr->next;
+        }
+        outStream << endl;
+    }
+
+    return outStream;
+}
+
+#endif
+
 #endif
